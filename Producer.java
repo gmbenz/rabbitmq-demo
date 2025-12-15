6b1ade6b4fdf188c8ecdcb53082a98d0dda606bd
Glenn Benz <glenn.benz@gmail.com>
2025-12-14 22:56:22 -0500

    Add a timestamp to the message

--- Producer.java
+++ Producer.java
@@ -1,6 +1,8 @@
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Producer {
     private final static String QUEUE_NAME = "testqueue";
@@ -16,8 +18,10 @@ public class Producer {
 
             channel.queueDeclare(QUEUE_NAME, true, false, false, null);
 
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
             for (int i = 1; i <= 10; i++) {
-                String message = "Hello message " + i;
+                String timestamp = LocalDateTime.now().format(formatter);
+                String message = "Hello message " + i + " at " + timestamp;
                 channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
                 System.out.println("Sent: " + message);
             }
